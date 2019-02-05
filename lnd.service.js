d548d660e5c5b722bab25fc13bd86ded692e7338
btarp <brandon.m.herbert@gmail.com>
2019-02-04 17:52:05 -0800

    lnd service

---
+++ lnd.service.js
@@ -0,0 +1,47 @@
+const request = require('request');
+const config = require('./config');
+
+exports.createInvoice = function() {
+
+    const promise = new Promise((resolve, reject) => {
+
+        const requestOptions = {
+            url: `${config.lndHost}/v1/invoices`,
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Grpc-Metadata-macaroon': config.macaroon
+            },
+            rejectUnauthorized: false,
+            body: JSON.stringify({
+                "private": false,
+                "memo": config.memo ,
+                "value": "100"
+            })
+        };
+        
+        request.post(requestOptions, (error, response, body) => {   
+            if(error) {
+                reject(error);
+            } else if(body) {
+                resolve(body);
+            } else {
+                reject('Something went wrong');
+            }         
+        });
+
+    });
+
+    return promise;
+
+}
+
+exports.createQR = function(response) {
+
+    const promise = new Promise((resolve, reject) => {
+
+    });
+
+    return promise;
+
+}

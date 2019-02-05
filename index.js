4f96d00a2ec9d0a1285db7b3c540166d80322962
btarp <brandon.m.herbert@gmail.com>
2019-02-05 14:32:05 -0800

    added path for created text invoices

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const logger = require('morgan');
 const express = require('express');
 const QRCode = require('qrcode');
@@ -7,6 +8,63 @@ const config = require('./config');
 const lndService = require('./lnd.service');
 
 app.use(logger('dev'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.get('/ln/text', (req, res) => {
+    lndService.createInvoice(config.requestAmount, config.memo)
+        .then(response => {
+            try {
+                var txData = JSON.parse(response);
+                if(!txData.payment_request) throw 'Error generating transaction';
+            } catch(err) {
+                res.render('text', { message: 'Error generating transaction' });
+                return lndService.sendImage(res, config.errorImage);
+            }
+            res.render('text', { message: txData.payment_request });
+        }, err => {            
+            res.render('text', { message: 'Error generating transaction' });
+        });    
+});
+
+app.get('/ln/text/:amount/:message', (req, res) => {
+
+    var amount = req.params.amount;
+    var message = req.params.message;
+
+    if(!amount || !message || isNaN(amount)) res.render('text', { message: 'Error generating transaction' });
+    else {
+        amount = parseInt(amount);
+        amount = Math.abs(amount);
+
+        if(amount > config.maxRequestAmount) {
+            amount = config.maxRequestAmount;
+        } else if(amount < config.minRequestAmount) {
+            amount = config.minRequestAmount;
+        }
+
+        if(message.length > 50) {
+            message = message.slice(0, 50);
+        }
+
+        lndService.createInvoice(amount, message)
+        .then(response => {
+            try {
+                var txData = JSON.parse(response);
+                if(!txData.payment_request) throw 'Error generating transaction';
+            } catch(err) {
+                res.render('text', { message: 'Error generating transaction' });
+                return lndService.sendImage(res, config.errorImage);
+            }
+            res.render('text', { message: txData.payment_request });
+        }, err => {            
+            res.render('text', { message: 'Error generating transaction' });
+        });    
+
+    }
+   
+});
 
 app.get('/ln/:amount/:message', (req, res) => {
 
